74c1352cb4aa1309f170e616f0979fc9e59841dc
Mark Lai <laimark900317@gmail.com>
2021-12-17 23:40:54 +0800

    建好分工 function query要寫在 services裡

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
-const quotesRouter = require('./routes/quotes');
+const port = 9000 || process.env.PORT;
+//const quotesRouter = require('./routes/quotes');
+const markRouter = require('./routes/mark');
+const nnRouter = require('./routes/nn');
+const niRouter = require('./routes/ni');
 
 app.use(express.json());
 
@@ -9,7 +12,10 @@ app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
 
-app.use('/quotes', quotesRouter);
+//app.use('/quotes', quotesRouter);
+app.use('/mark', markRouter);
+app.use('/nn', nnRouter);
+app.use('/ni', niRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
