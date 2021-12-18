3121e14789150017a47dfb8afaa8cde2681cc72d
24135768b <58065881+24135768b@users.noreply.github.com>
2021-12-18 22:01:18 +0800

    加入寫死的example data到table裡面

--- frontend/src/components/BuyTable.vue
+++ frontend/src/components/BuyTable.vue
@@ -1,11 +1,44 @@
-<template lang="">
-  <div>
-    this is table -------
-  </div>
+<template>
+  <el-table :data="productTable" style="width: 100%">
+    <el-table-column prop="product" label="Product" width="150" />
+    <el-table-column prop="shop" label="Shop" width="150" />
+    <el-table-column prop="remaining" label="Remaining" width="150"/>
+    <el-table-column prop="price" label="Price" width="150"/>
+  </el-table>
 </template>
-<script>
+
+<script >
 export default {
-  
+  data() {
+    return {
+      productTable: [
+        {
+          product: 'apple',
+          shop: 'qwer',
+          remaining: '24',
+          price: '20',
+        },
+        {
+          product: 'orange',
+          shop: 'asdf',
+          remaining: '44',
+          price: '25',
+        },
+        {
+          product: 'banna',
+          shop: 'zxcv',
+          remaining: '251',
+          price: '40',
+        },
+        {
+          product: 'dick',
+          shop: 'Tom',
+          remaining: '642',
+          price: '1999'
+        },
+      ],
+    }
+  },
 }
 </script>
 <style lang="">
