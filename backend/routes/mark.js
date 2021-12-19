3827476b523c0478efefc1ec4d02284c47560074
Mark Lai <laimark900317@gmail.com>
2021-12-19 11:52:38 +0800

    backend create new customer

--- backend/routes/mark.js
+++ backend/routes/mark.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mark = require('../services/mark');
 
-
+router.post('/register/Customer', function(req, res, next) {
+  try {
+    res.json(mark.createNewCustomer(req.body));
+  } catch(err) {
+    console.error(`Error while adding customer `, err.message);
+    next(err);
+  }
+});
 
 
 module.exports = router;
--- backend/services/mark.js
+++ backend/services/mark.js
@@ -1,6 +1,17 @@
 const db = require('../services/db'); // dp 裡的function 用ㄌㄞquery 和 改資料的
 
 
+function createNewCustomer(data){
+  const {Email, Name, PhoneNumber} = data
+  const result = db.run('INSERT INTO Customer (CustomerID, Name, PhoneNumber) VALUES (@Email, @Name, @PhoneNumber)', {Email, Name, PhoneNumber});
+  let message = 'Error in creating Customer';
+  if (result.changes) {
+    message = 'Customer created successfully';
+  }
+  return {message};
+}
+
+
 module.exports = {
-  //function 名字  參考 quotes
+  createNewCustomer,
 }
