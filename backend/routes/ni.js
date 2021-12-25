0464cfef2c533cb23639d0df70d4a0bc7439a009
charlieni <76686807+charlieni@users.noreply.github.com>
2021-12-26 05:05:59 +0800

    Update ni.js

--- backend/routes/ni.js
+++ backend/routes/ni.js
@@ -1,6 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const ni = require('../services/ni');
 
+router.get('/Revenue', function(req, res, next) {
+    try {
+      res.json(ni.Revenue(req.query.ManagerID));
+    } catch(err) {
+      console.error(`Error while getting revenue `, err.message);
+      next(err);
+    }
+  });
+router.get('/TradeHistory', function(req, res, next) {
+    try {
+    res.json(ni.TradeHistory(req.query.ManagerID,req.query.page));
+    } catch(err) {
+    console.error(`Error while getting trade_history `, err.message);
+    next(err);
+    }  
+});
+router.get('/Shop', function(req, res, next) {
+    try {
+    res.json(ni.Shop(req.query.ManagerID));
+    } catch(err) {
+    console.error(`Error while getting shop `, err.message);
+    next(err);
+    }  
+});
+router.get('/OrderHistory', function(req, res, next) {
+    try {
+    res.json(ni.OrderHistory(req.query.ManagerID,req.query.page));
+    } catch(err) {
+    console.error(`Error while getting order_history `, err.message);
+    next(err);
+    }  
+});
+router.get('/Order', function(req, res, next) {
+    try {
+    res.json(ni.Order(req.query.ManagerID,req.query.page));
+    } catch(err) {
+    console.error(`Error while getting order `, err.message);
+    next(err);
+    }  
+});
+router.get('/Page', function(req, res, next) {
+    try {
+    res.json(ni.Page(req.query.ManagerID,req.query.page));
+    } catch(err) {
+    console.error(`Error while getting page `, err.message);
+    next(err);
+    }  
+});
+router.post('/register/Manager', function(req, res, next) {
+  try {
+    res.json(ni.CreateNewManager(req.body));
+  } catch(err) {
+    console.error(`Error while adding Manager `, err.message);
+    next(err);
+  }
+});
 
 
 module.exports = router;
