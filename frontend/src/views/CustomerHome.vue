b64b6229bde6fc33023f7c4546c00fd3d45ed488
24135768b <58065881+24135768b@users.noreply.github.com>
2022-01-19 13:21:48 +0800

    增加boarder長度

--- frontend/src/views/CustomerHome.vue
+++ frontend/src/views/CustomerHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-container style="height: 1000px; border: 1px solid #eee">
     
     <el-container>
       <el-header style="font-size: 30px; text-align: left">
